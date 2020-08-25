95f2361564486f60cdec4f32f16aefc73e0e502e
Bryon <bryon.pectol@gmail.com>
2020-08-24 22:22:23 -0600

    almost done with create!

--- js/create.js
+++ js/create.js
@@ -1,16 +1,53 @@
+
 function GetInput() {
     let container = document.createElement("div");
     let firstnameVal = document.getElementById("firstname").value;
     let lastnameVal = document.getElementById("lastname").value;
-    let birthdayVal = document.getElementById("day").value;
     let monthVal = document.getElementById("month").value;
 
-    document.body.appendChild(container);
-    container.appendChild(firstnameVal);
-    container.appendChild(lastnameVal);
-    container.appendChild(birthdayVal);
+    if (monthVal == "December") {
+        bendertype = "Water";
+    }
+   if (monthVal == "January") {
+       bendertype = "Water";
+   }
+   if (monthVal == "Feburary") {
+    bendertype = "Water";
+}
+if (monthVal == "March") {
+    bendertype = "Earth";
+    home = "Ba Sing Se";
+}
+if (monthVal == "April") {
+    bendertype = "Earth";
+    home = "Omashu"
+}
+if (monthVal == "May") {
+    bendertype = "Earth";
+}
+if (monthVal == "June") {
+    bendertype = "Fire";
+}
+if (monthVal == "July") {
+    bendertype = "Fire";
+}
+if (monthVal == "August") {
+    bendertype = "Fire";
+}
+if (monthVal == "September") {
+    bendertype = "Air";
+}
+if (monthVal == "October") {
+    bendertype = "Air";
+}
+if (monthVal == "November") {
+    bendertype = "Air";
+}
     
-    alert("Your name is: " + firstnameVal + " " + lastnameVal + ", and your birthday is: " + monthVal + " " + birthdayVal + ".");
+
+    document.write("Hello, " + firstnameVal + " " + lastnameVal + ". You are a(n) " + bendertype + " bender who lives in " +  +);
+    
+    // alert("Your name is: " + firstnameVal + " " + lastnameVal + ", and your birthday is: " + monthVal + " " + birthdayVal + ".");
 
     console.log(firstnameVal + " " + lastnameVal + " : " + birthdayVal + " " + monthVal);
 }

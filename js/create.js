b21626362a4668018721ddb208671a2ea9f96d9f
Bryon <bryon.pectol@gmail.com>
2020-08-27 16:25:25 -0600

    added lovers

--- js/create.js
+++ js/create.js
@@ -18,9 +18,6 @@
 
     let homeImg = document.getElementById("homeimg");
 
-    let Email = document.getElementById("email").value;
-    let Emailstore = localStorage.setItem("email", Email);
-
 
     let waterHome = "/html/water.html";
     let earthHome = "/html/earth.html";
@@ -45,18 +42,7 @@ function GetInput() {
     let lastnameVal = document.getElementById("lastname").value;
     let monthVal = document.getElementById("month").value;
     let ageVal = document.getElementById("age").value;  
-
-// email
-    
-
-
-   if(Email = Emailstore)
-        {   
-            alert("You Already have an email")
-        }
-        else {
-            Emailstore = localStorage.setItem("email", Email);
-        }
+    let Gender = document.getElementById("gender").value;
 
 
 
@@ -68,16 +54,41 @@ function GetInput() {
 
     let Form = document.getElementById("mainform");
 
-const earthhome = ["Ba Sing Se", "Omashu", "Kyoshi Island", "Chin Village", "Gaoling", "Plains Village"];
-const firehome = ["Ember Island", "Fire Fountain City", "The Fire Nation Capital", "Jang Hui", "Shu Jing"];
-const waterhome = ["The Foggy Swamp", "The Northern Water Tribe", "The Southern Water Tribe"];
-const airhome = ["The Northern Air Temple", "The Eastern Air Temple", "The Southern Air Temple", "The Western Air Temple"];
+const earthhome = ["Ba Sing Se", "Omashu", "Kyoshi Island", "Chin Village", "Gaoling", "Plains Village", "Senlin Village", "Makapu Village", "The outskirts"];
+const firehome = ["Ember Island", "Fire Fountain City", "The Fire Nation Capital", "Jang Hui", "Shu Jing", "The outskirts"];
+const waterhome = ["The Foggy Swamp", "The Northern Water Tribe", "The Southern Water Tribe", "The outskirts"];
+const airhome = ["The Northern Air Temple", "The Eastern Air Temple", "The Southern Air Temple", "The Western Air Temple", "The outskirts"];
 
-const job = ["a soldier for the military", "a fisherman", "a blacksmith", "a royal", "a peasant", "a tea maker", "an entertainer", "a general for the military", "a merchant", "a pirate", "a miner"];
+const job = ["a soldier for the military", "a fisherman", "a blacksmith", "a royal", "a peasant", "a tea maker", "an entertainer", "a general for the military", "a merchant", "a pirate", "a miner", "a bending instructor", "a doctor", "a sailor", "a chef", "a housemaid", "an inn keeper", "an inventor", "a circus performer"];
 
 const era = ["Era of Raava", "First Age of the Avatar", "Second Age of the Avatar", "Harmonic Convergence", "Hundred Year War", "Anti-bending Revolution"]
 
 const homeIMG = ["/images/nwt.png", "/images/swt.png", "/images/tfs.png"];
+
+const malelove = ["Lee", "Guko", "San", "Fred", "Shang", "Uzhan", "Yohmi", "Kim", "Jin"];
+const maleloveLast = [" Yang", " Wu", " Qino", " Beifong", " Xiao", " Sonar", " Li", " Chen"];
+const femalelove = ["Yang", "Kora", "Julie", "Aiko", "Kim", "Gia", "Aya", "Mei", "Tai"]
+const femaleloveLast = [" Yong", " Wu", " Qino", " Beifong", " Xiao", " Sonar", " Li", " Chen"]
+
+let randomLoverMale = malelove[Math.floor(Math.random() * malelove.length)];
+let randomLoverMaleLast = maleloveLast[Math.floor(Math.random() * maleloveLast.length)];
+
+let randomLoverFemale = femalelove[Math.floor(Math.random() * femalelove.length)];
+let randomLoverFemaleLast = femaleloveLast[Math.floor(Math.random() * femaleloveLast.length)];
+
+
+if(Gender == "female") {
+    oppo = "man";
+    lover = randomLoverMale + randomLoverMaleLast;
+}
+
+if(Gender == "male") {
+    oppo = "woman";
+    lover = randomLoverFemale + randomLoverFemaleLast;
+}
+
+let LoverDES = localStorage.setItem("Lovers", lover);
+let opposite = localStorage.setItem("op", oppo);
 
 
 let earthHOME = earthhome[Math.floor(Math.random() * earthhome.length)];
@@ -118,7 +129,7 @@ let avatarDes = localStorage.setItem("avatardes", avatarChoose);
     
 
     if (monthVal == "December") {
-        bendertype = "a Water";
+        bendertype = "Water";
         home = waterHOME;
         document.body.style.backgroundImage = "url('/images/bgs/water.jpg')";
         document.body.style.backgroundSize = "cover";
@@ -126,7 +137,7 @@ let avatarDes = localStorage.setItem("avatardes", avatarChoose);
         let homeLink = localStorage.setItem("homelink", waterHome);
     }
    if (monthVal == "January") {
-       bendertype = "a Water";
+       bendertype = "Water";
        home = waterHOME;
        document.body.style.backgroundImage = "url('/images/bgs/water.jpg')";
        document.body.style.backgroundSize = "cover";
@@ -134,7 +145,7 @@ let avatarDes = localStorage.setItem("avatardes", avatarChoose);
        let homeLink = localStorage.setItem("homelink", waterHome);
    }
    if (monthVal == "Feburary") {
-    bendertype = "a Water";
+    bendertype = "Water";
     home = waterHOME;
     document.body.style.backgroundImage = "url('/images/bgs/water.jpg')";
     document.body.style.backgroundSize = "cover";
@@ -142,7 +153,7 @@ let avatarDes = localStorage.setItem("avatardes", avatarChoose);
     let homeLink = localStorage.setItem("homelink", waterHome);
 }
 if (monthVal == "March") {
-    bendertype = "an Earth";
+    bendertype = "Earth";
     home = earthHOME;
     document.body.style.backgroundImage = "url('/images/bgs/earth.jpg')";
     document.body.style.backgroundSize = "cover";
@@ -150,7 +161,7 @@ if (monthVal == "March") {
     let homeLink = localStorage.setItem("homelink", earthHome);
 }
 if (monthVal == "April") {
-    bendertype = "an Earth";
+    bendertype = "Earth";
     home = earthHOME;
     document.body.style.backgroundImage = "url('/images/bgs/earth.jpg')";
     document.body.style.backgroundSize = "cover";
@@ -158,7 +169,7 @@ if (monthVal == "April") {
     let homeLink = localStorage.setItem("homelink", earthHome);
 }
 if (monthVal == "May") {
-    bendertype = "an Earth";
+    bendertype = "Earth";
     home = earthHOME;
     document.body.style.backgroundImage = "url('/images/bgs/earth.jpg')";
     document.body.style.backgroundSize = "cover";
@@ -166,7 +177,7 @@ if (monthVal == "May") {
     let homeLink = localStorage.setItem("homelink", earthHome);
 }
 if (monthVal == "June") {
-    bendertype = "a Fire";
+    bendertype = "Fire";
     home = fireHOME;
     document.body.style.backgroundImage = "url('/images/bgs/fire.png')";
     document.body.style.backgroundSize = "cover";
@@ -174,7 +185,7 @@ if (monthVal == "June") {
     let homeLink = localStorage.setItem("homelink", fireHome);
 }
 if (monthVal == "July") {
-    bendertype = "a Fire";
+    bendertype = "Fire";
     home = fireHOME;
     document.body.style.backgroundImage = "url('/images/bgs/fire.png')";
     document.body.style.backgroundSize = "cover";
@@ -182,7 +193,7 @@ if (monthVal == "July") {
     let homeLink = localStorage.setItem("homelink", fireHome);
 }
 if (monthVal == "August") {
-    bendertype = "a Fire";
+    bendertype = "Fire";
     home = fireHOME;
     document.body.style.backgroundImage = "url('/images/bgs/fire.png')";
     document.body.style.backgroundSize = "cover";
@@ -190,7 +201,7 @@ if (monthVal == "August") {
     let homeLink = localStorage.setItem("homelink", fireHome);
 }
 if (monthVal == "September") {
-    bendertype = "an Air";
+    bendertype = "Air";
     home = airHOME;
     document.body.style.backgroundImage = "url('/images/bgs/air.jpg')";
     document.body.style.backgroundSize = "cover";
@@ -198,7 +209,7 @@ if (monthVal == "September") {
     let homeLink = localStorage.setItem("homelink", airHome);
 }
 if (monthVal == "October") {
-    bendertype = "an Air";
+    bendertype = "Air";
     home = airHOME;
     document.body.style.backgroundImage = "url('/images/bgs/air.jpg')";
     document.body.style.backgroundSize = "cover";
@@ -206,7 +217,7 @@ if (monthVal == "October") {
     let homeLink = localStorage.setItem("homelink", airHome);
 }
 if (monthVal == "November") {
-    bendertype = "an Air";
+    bendertype = "Air";
     home = airHOME;
     document.body.style.backgroundImage = "url('/images/bgs/air.jpg')";
     document.body.style.backgroundSize = "cover";
@@ -216,6 +227,8 @@ if (monthVal == "November") {
 
 // let HomeIMG = homeIMG[Math.floor(Math.random() * homeIMG.length)];
 let Job = localStorage.setItem("job", JOB);
+
+let genderDES = localStorage.setItem("gender", Gender);
 
 
 
@@ -249,12 +262,12 @@ if (avatarChoose > 42 && avatarChoose <= 100) {
 
     
 
-    container1 = document.getElementById("aboutyou").innerHTML = "Hello, " + firstnameVal + " " + lastnameVal + ". You are " + bendertype + " bender who was born in " + home + ". You are " + ageVal + " years old. There is a 1 in 7 billion chance that you will be the next Avatar and, you are  " + avatarChoice + " You are also " + JOB + "." ;
+    container1 = document.getElementById("aboutyou").innerHTML = "Hello, " + firstnameVal + " " + lastnameVal + ". You are a " + Gender + " " + bendertype + " bender who was born in " + home + ". You are " + ageVal + " years old. There is a 1 in 7 billion chance that you will be the next Avatar and, you are  " + avatarChoice + " You are also " + JOB + "." ;
 
     // NextBtn1.style.display = "block";
 
     // alert("Your name is: " + firstnameVal + " " + lastnameVal + ", and your birthday is: " + monthVal + " " + birthdayVal + ".");
-    console.log(avatarChoose + " : " + avatarChoice + " : " + waterHOME + " : " + homeImg);
+    console.log(avatarChoose + " : " + avatarChoice + " : " + waterHOME + " : " + homeImg + " : " + Gender);
 }
 
 function secondInput() {
--- js/earth.js
+++ js/earth.js
@@ -6,6 +6,8 @@ let bender=localStorage.getItem("bendertype");
 let imageHome=localStorage.getItem("homeimage");
 let avatarDES=localStorage.getItem("avatardes");
 let jobb = localStorage.getItem("job");
+let LOVERS = localStorage.getItem("Lovers");
+let Opposite = localStorage.getItem("op");
 
 console.log(name + lastname + month + age + imageHome);
 
@@ -15,5 +17,5 @@ let container = document.getElementById("aboutYOU").innerHTML = ""+ name + ", " 
 
 // IMage.setAttribute('src', imageHome);
 
-let secondCon = document.getElementById("ABOUTyou").innerHTML = "Your chance of becoming the Avatar was: " + avatarDES + " / 7000000000 (billion), Sorry. But remember, you are still " + jobb + ".";
+let secondCon = document.getElementById("ABOUTyou").innerHTML = "Your chance of becoming the Avatar was: " + avatarDES + " / 7000000000 (billion), Sorry. But remember, you are still " + jobb + ". During your life, you marry a " + Opposite + " named, " + LOVERS + ".";
 
